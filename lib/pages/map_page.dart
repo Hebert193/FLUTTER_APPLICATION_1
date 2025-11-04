de637363c4c5b35b831f089d61201c5722017222
HEBERT FERREIRA MOREIRA DOS SANTOS <hebert.fmsantos@senacsp.edu.br>
2025-11-03 21:52:36 -0300

    atualização

--- lib/pages/map_page.dart
+++ lib/pages/map_page.dart
@@ -1,22 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:flutter_map/flutter_map.dart';
+import 'package:latlong2/latlong.dart';
 
-class MapPage extends StatelessWidget {
-  const MapPage({super.key});
+class FlutterMapPage extends StatefulWidget {
+  const FlutterMapPage({super.key});
 
   @override
+  State<FlutterMapPage> createState() => _FlutterMapPageState();
+}
+
+class _FlutterMapPageState extends State<FlutterMapPage> {
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Mapa com Marcações'),
+    return FlutterMap(
+      options: MapOptions(
+        initialCenter: LatLng(
+          51.509364,
+          -0.128928,
+        ), // Center the map over London
+        initialZoom: 9.2,
       ),
-      body: const GoogleMap(
-        initialCameraPosition: CameraPosition(
-          target: LatLng(-23.5275, -46.6800), // Senac Lapa Tito
-          zoom: 14,
+      children: [
+        TileLayer(
+          // Bring your own tiles
+          urlTemplate:
+              'https://tile.openstreetmap.org/{z}/{x}/{y}.png', // For demonstration only
+          userAgentPackageName: 'com.example.app', // Add your app identifier
+          // And many more recommended properties!
         ),
-        
-      ),
+        RichAttributionWidget(
+          // Include a stylish prebuilt attribution widget that meets all requirments
+          attributions: [
+            TextSourceAttribution(
+              'OpenStreetMap contributors',
+              onTap: () {}, // (external)
+            ),
+            // Also add images...
+          ],
+        ),
+      ],
     );
   }
 }
